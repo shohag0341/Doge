5c02c5d32e92be7261e22955d94c260b23113e6c
shohag0341 <shohag26357@gmail.com>
2026-09-03 12:06:27 +0600

    Update botUsername in SUPABASE_CONFIG

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@ const SUPABASE_CONFIG = {
     url: 'https://znkugpdxnhecymzhveus.supabase.co',
     anonKey: 'sb_publishable_Pl1ZB0cvuIpcT_DVGqDZ2w_nESubl9l',
     adminIds: ['5371043649'],
-    botUsername: '@dogecoin_247bot'
+    botUsername: 'dogecoin_247bot'
 };
